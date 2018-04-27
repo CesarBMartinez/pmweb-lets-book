53c22fc079bce62a5abd67ab3cf40b59b2b0f5fe
CesarBMartinez <cesar_martinez22@hotmail.com>
2018-04-27 15:22:35 -0300

    change source.js structure

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ console.log("Lendo arquivos...");
 const style = fs.readFileSync('./build/css/style.css', 'utf8');
 const html = fs.readFileSync('./build/html/modal.html', 'utf8');
 const sourceJS = fs.readFileSync('./build/js/source.js', 'utf8');
+
 console.log("Concatenando arquivos...");
-const buildJS = `var style = '<style>${style}</style>';var modalHtml = '${html}';${sourceJS}`;
+const buildJS = `var style='<style>${style}</style>';var modalHtml='${html}';${sourceJS}`;
 
 console.log("Criando build.js...");
 fs.writeFileSync('./build/js/build.js', buildJS, 'utf8');
+
 console.log("Done!");
--- src/js/source.js
+++ src/js/source.js
@@ -1,45 +1,33 @@
+/* Adiciona todo o CSS necessário */
 function addCss() {
 	if (typeof style !== 'undefined') {
 		$('body').append(style);
 	}
 }
 
+/* Adiciona o html da modal */
 function addModalHtml() {
 	if (typeof modalHtml !== 'undefined') {
 		$('body').append(modalHtml);
 	}
 }
 
-addCss();
-addModalHtml();
+/* Altera a logo da Pmweb */
+function logo() {
+	//URL da nova logo
+	var logo = "https://i.imgur.com/KYSi55p.png";
+	
+	//Mudando logo do header
+	$('#navContent a img').attr("src",logo);
 
-//Mudando logo do header
-$('#navContent a img').attr("src","https://i.imgur.com/KYSi55p.png");
-
-//Juntando os fields de partida e chegada
-var dataSaida = $("#var-partida-pariodo-label").clone();
-var labelNoites = "#var-periodo-label-noites";
-var labelNoitesNew = "#var-periodo-label-noites-2";
-var diarias = $(labelNoites).clone();
-
-$(".chegada-field .busca-field-label").text("Período");
-$("#var-chegada-periodo-label").after(dataSaida);
-$(".chegada-field").append(diarias);
-$("#var-partida-pariodo-label").after("<span id='var-periodo-label-noites-2'></span>");
-calculaNoites();
-
-/* chama a função calculaNoites sempre que o elemento #var-periodo-label-noites mudar */
-$(labelNoites).bind('DOMNodeInserted DOMNodeRemoved',function(){
-  calculaNoites();
-});
-$(".saida-field").remove();
-
-//Sobrescreve a função limparCalendario
-limparCalendario = function(){return;}
+	//Adicionando a logo no footer
+	$('.footerFirstLine .selo-lb').html("").append('<a href="/D/"><img src="'+ logo +'"></a>');
+}
 
 /* substitui a palavra 'diária(s)' por 'noite(s)' */
 function calculaNoites() {
-  var noitesTxt = $(labelNoites).text();
+	var labelNoitesNew = "#var-periodo-label-noites-2";
+  var noitesTxt = $("#var-periodo-label-noites").text();
   noitesTxt = (noitesTxt.charAt(noitesTxt.length-1) === "s") ? noitesTxt.replace("diárias", "noites") : noitesTxt.replace("diária", "noite");
   
   if (noitesTxt) {
@@ -50,6 +38,58 @@ function calculaNoites() {
   }
 }
 
+/* Junta os fields de partida e chegada */
+function fieldPeriodo() {
+	var dataSaida = $("#var-partida-pariodo-label").clone();
+	var labelNoites = "#var-periodo-label-noites";
+	var labelNoitesNew = "#var-periodo-label-noites-2";
+	var diarias = $(labelNoites).clone();
+
+	$(".chegada-field .busca-field-label").text("Período");
+	$("#var-chegada-periodo-label").after(dataSaida);
+	$(".chegada-field").append(diarias);
+	$("#var-partida-pariodo-label").after("<span id='var-periodo-label-noites-2'></span>");
+	
+	calculaNoites();
+	// chama a função calculaNoites sempre que o elemento #var-periodo-label-noites mudar
+	$(labelNoites).bind('DOMNodeInserted DOMNodeRemoved',function(){
+	  calculaNoites();
+	});
+
+	//Remove campo de saida
+	$(".saida-field").remove();
+
+	//Sobrescreve a função limparCalendario
+	limparCalendario = function(){return;};
+}
+
+function alteraCalendario() {
+	//sobrescreve a função loadCalendarUm
+  loadCalendarUm = function(date) {
+    //Desabilita a flecha da esquerda caso o mes desta data seja o mês atual
+    if (date.getMonth() == new Date().getMonth() && date.getFullYear() == new Date().getFullYear()) {
+      $(".arrow-left").css("pointer-events", "none").css("opacity", "0.2");
+    }
+    else {
+      $(".arrow-left").css("pointer-events", "").css("opacity", "1");
+    }
+
+    var dataMes = date.getMonth() + 1;
+    var dataAno = date.getFullYear();
+
+    //CARREGA O CALENDARIO
+    $("#calendario-1").html(htmlCalendario(dataMes, dataAno));
+
+    bindCalendarEvents();
+    fillBestRates();
+    calendarioExiste = true;
+  }
+
+  // Remove calendario 2
+  $("#calendario-2").remove();
+}
+
+/* Valida se a idade das crianças foram preenchidas */
 function validaIdadeCriancas() {
 	var criancasQtd = $("#criancas-quantidade-var");
 	var criancasVal = $("#var-busca-criancas").val();
@@ -62,6 +102,7 @@ function validaIdadeCriancas() {
 		return true;
 	});
 
+	//Sobrescreve função novaBusca, adicionando a validação da idade das crianças
 	novaBusca = function() {
 		criancasQtd = $("#criancas-quantidade-var").html();
 		criancasVal = $("#var-busca-criancas").val();
@@ -73,6 +114,17 @@ function validaIdadeCriancas() {
 			$("#var-busca-tarifa").val("");
 			$("#form-nova-busca").submit();
 		}
-	}
+	};
 }
-validaIdadeCriancas();
+
+/*
+ * Chama as funções necessárias
+ */
+(function load() {
+	addCss();
+	logo();
+	fieldPeriodo();
+	alteraCalendario();
+	validaIdadeCriancas();
+	addModalHtml();
+})();
